abfd0cf9b27cd77715a9c17bc1580c9922d651d9
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-09-26 20:06:59 -0400

    Created speech_tokens function, including helper function and example, to create tidy tokenized tibble of text of a speech from the web

---
+++ R/speech_tokens.R
@@ -0,0 +1,26 @@
+#' Speech Tokenizer
+#'
+#' speech_tokens function receives a url input and uses CSS selectors
+#' to parse and tokenize the portion of the target url believed to contain the
+#' txt of a speech; returns a tibble containing each word in that speech
+#'
+#' @param url
+#'
+#' @return
+#' @export
+#' @importFrom magrittr %>%
+#'
+#' @examples
+#' dream <- speech_tokens("https://kinginstitute.stanford.edu/king-papers/documents/i-have-dream-address-delivered-march-washington-jobs-and-freedom")
+#'
+#' dream
+#'
+speech_tokens <- function(url){
+  xml2::read_html(url) %>%
+    rvest::html_nodes("#block-system-main p+ p") %>%
+    rvest::html_text() %>%
+    stringr::str_replace_all("\\(([^)]*)\\)", "") %>%
+    stringr::str_replace_all("\\[([^]]*)\\]", "") %>%
+    tibble::as_tibble() %>%
+    tidytext::unnest_tokens(word, value)
+}
