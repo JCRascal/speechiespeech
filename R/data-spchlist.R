92e791a25fee0a184f5eb57d5b11ef422184de49
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-14 17:13:17 -0400

    Created documentation for spchlist dataset, including information about source documents

---
+++ R/data-spchlist.R
@@ -0,0 +1,16 @@
+#' Speech list
+#'
+#' List of local html files containing text of speeches by prominent public
+#'   figures.
+#'
+#' Sources include:
+#'   \itemize{
+#'     \item kinginstitute.stanford.edu
+#'     \item er.jsc.nasa.gov
+#'     \item ourdocuments.gov
+#'     \item americanrhetoric.com
+#'     \item jfklibrary.org
+#'     \item teachingamericanhistory.org
+#'     \item presidency.ucsb.edu
+#'   }
+"spchlist"
