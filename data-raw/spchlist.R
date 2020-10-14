e9cd9ba9a6eda4ff85ad582875935a288afc2d68
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-14 11:34:43 -0400

    Updated spchlist and sample_speech_data datasets to reference local copies of web pages to minimize downloads

--- data-raw/spchlist.R
+++ data-raw/spchlist.R
@@ -1,15 +1,26 @@
+# url <- c("https://kinginstitute.stanford.edu/king-papers/documents/i-have-dream-address-delivered-march-washington-jobs-and-freedom",
+#           "https://kinginstitute.stanford.edu/king-papers/documents/beyond-vietnam",
+#           "https://kinginstitute.stanford.edu/our-god-marching",
+#          "https://kinginstitute.stanford.edu/king-papers/documents/ive-been-mountaintop-address-delivered-bishop-charles-mason-temple",
+#          "https://er.jsc.nasa.gov/seh/ricetalk.htm",
+#          "https://www.ourdocuments.gov/doc.php?flash=false&doc=91&page=transcript",
+#          "https://www.americanrhetoric.com/speeches/jfkcommonwealthmass.htm",
+#          "https://www.jfklibrary.org/archives/other-resources/john-f-kennedy-speeches/american-newspaper-publishers-association-19610427",
+#          "https://teachingamericanhistory.org/library/document/great-society-speech/",
+#          "https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-0",
+#          "https://www.presidency.ucsb.edu/documents/radio-and-television-remarks-upon-signing-the-civil-rights-bill")
 
-url <- c("https://kinginstitute.stanford.edu/king-papers/documents/i-have-dream-address-delivered-march-washington-jobs-and-freedom",
-          "https://kinginstitute.stanford.edu/king-papers/documents/beyond-vietnam",
-          "https://kinginstitute.stanford.edu/our-god-marching",
-         "https://kinginstitute.stanford.edu/king-papers/documents/ive-been-mountaintop-address-delivered-bishop-charles-mason-temple",
-         "https://er.jsc.nasa.gov/seh/ricetalk.htm",
-         "https://www.ourdocuments.gov/doc.php?flash=false&doc=91&page=transcript",
-         "https://www.americanrhetoric.com/speeches/jfkcommonwealthmass.htm",
-         "https://www.jfklibrary.org/archives/other-resources/john-f-kennedy-speeches/american-newspaper-publishers-association-19610427",
-         "https://teachingamericanhistory.org/library/document/great-society-speech/",
-         "https://www.presidency.ucsb.edu/documents/address-before-joint-session-the-congress-0",
-         "https://www.presidency.ucsb.edu/documents/radio-and-television-remarks-upon-signing-the-civil-rights-bill")
+url <- c(system.file("speeches", "dream.txt", package = "speechiespeech"),
+         system.file("speeches", "vietnam.txt", package = "speechiespeech"),
+         system.file("speeches", "god_marching.txt", package = "speechiespeech"),
+         system.file("speeches", "mountaintop.txt", package = "speechiespeech"),
+         system.file("speeches", "to_the_moon.txt", package = "speechiespeech"),
+         system.file("speeches", "JFK_inaugural.txt", package = "speechiespeech"),
+         system.file("speeches", "city_hill.txt", package = "speechiespeech"),
+         system.file("speeches", "president_press.txt", package = "speechiespeech"),
+         system.file("speeches", "great_society.txt", package = "speechiespeech"),
+         system.file("speeches", "post_JFK_death.txt", package = "speechiespeech"),
+         system.file("speeches", "civil_rights_act.txt", package = "speechiespeech"))
 
 title <- c("I Have a Dream",
             "Beyond Vietnam",
@@ -21,7 +32,7 @@ title <- c("I Have a Dream",
            "The President and the Press",
            "The Great Society",
            "Congressional Address Following Assasinaton of President Kennedy",
-           "1964Civil Rights Bill Signing Statement")
+           "1964 Civil Rights Bill Signing Statement")
 
 author <- c("Dr. Martin Luther King, Jr.",
              "Dr. Martin Luther King, Jr.",
