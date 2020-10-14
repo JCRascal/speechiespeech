aedfe1395274b97936b703e31b1dc472f7c48abc
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-14 16:00:51 -0400

    Created unit test that is (correctly) passed by build machine that verifies speechscrape on a local copy of I Have a Dream by matching against the value in sample_speech_data

--- tests/testthat/test-speechscrape.R
+++ tests/testthat/test-speechscrape.R
@@ -1,5 +1,11 @@
 context("Speech Scrape")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("speechscrape works with a local file", {
+  a <- speechscrape(system.file("speeches", "dream.txt", package = "speechiespeech"),
+                    slctr = spchlist$slctr[[1]])
+
+  b <- sample_speech_data$text[[1]]
+
+  c <- Reduce(setdiff, strsplit(c(a, b), split = ""))
+  expect_identical(c, character(0))
 })
