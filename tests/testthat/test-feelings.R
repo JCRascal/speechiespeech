9616e71522180c8475e1adbfbcaf928ac4b0a558
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-21 15:40:00 -0400

    Removed the kittens' fine work rebuilding test-feelings.

--- tests/testthat/test-feelings.R
+++ tests/testthat/test-feelings.R
@@ -3,25 +3,12 @@ test_that("feelings output includes two columns named word and sentiment", {
     speech_tokens_dfr() %>%
     feelings()
 
-<<<<<<< HEAD
+
   expect_equal(ncol(feels), 2)
   expect_equal(names(feels[1]), "word")
   expect_equal(names(feels[2]), "sentiment")
 
-||||||| 90f6d5a
-  expect_equal(ncol(feels) > 2, TRUE)
-  expect_equal(is.null(feels$word), FALSE)
-  expect_equal(is.null(feels$sentiment), FALSE)
-=======
-  expect_equal(ncol(feels), 2)
-  expect_equal(names(feels[1]), "word")
-  expect_equal(names(feels[2]), "sentiment")
 
-})
-
-test_that("", {
-
->>>>>>> 8da48062ff092584b8ffc797205ec33f81bc8c3b
 })
 
 
