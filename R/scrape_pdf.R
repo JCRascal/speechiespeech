a409abc2c46d3a6d28418b4d630429b01d85dc3c
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-30 15:06:20 -0400

    Created helpers for scrape_pdf that extract from the header of each page a column containing the type and author of the opinion; passed the tests in test-scrape_pdf that verify functionality of these helpers

--- R/scrape_pdf.R
+++ R/scrape_pdf.R
@@ -14,9 +14,31 @@ scrape_1 <- function(main_url, page){
     purrr::map(pdftools::pdf_text)
 }
 
-scrape_2 <- function(.data){
-#  .data$text %>%
+# scrape_optype receives a character vector representing a single page of a
+# Supreme Court slip opinion pdf and returns a one-column tibble which describes
+# the type of Supreme Court opinion contained in that page
 
+scrape_optype <- function(.data){
+  .data %>%
+    tibble::tibble("text" = .) %>%
+    purrr::map_dfr(stringr::str_trunc, 115) %>%
+    mutate(dissenting = stringr::str_detect(text, "dissenting")) %>%
+    mutate(per_curiam = stringr::str_detect(text, "Per Curiam")) %>%
+    mutate(opinion_type = if_else(dissenting == TRUE, true = "Dissenting", false = NA_character_)) %>%
+    mutate(opinion_type = if_else(per_curiam == TRUE, true = "Per Curiam", false = opinion_type)) %>%
+    select(opinion_type)
+
+}
+
+scrape_author <- function(.data){
+  .data %>%
+    tibble::tibble("text" = .) %>%
+    purrr::map_dfr(stringr::str_trunc, 115) %>%
+    mutate(author = stringr::str_extract(text, ".+(?=, J., dissenting)")) %>%
+    mutate(author = if_else(stringr::str_detect(text, "Per Curiam"), true = "Per Curiam", false = author)) %>%
+    select(author) %>%
+    purrr::map_dfr(stringr::str_trim) %>%
+    purrr::map_dfr(stringr::str_to_title)
 }
 
 organizer <- function(text){
--- tests/testthat/test-scrape_pdf.R
+++ tests/testthat/test-scrape_pdf.R
@@ -8,8 +8,33 @@ test_that("scrape_pdf returns a tibble of opinion, case name, opinion type, and 
   expect_equal(length(scrapings), 4)
 })
 
-test_that("scrape_2 assigns an opinion type and author for each page in a source pdf", {
+test_that("scrape_head assigns correct opinion type and author for each page in a source pdf", {
+  tester <- scrape_head(test_pdf[[1]])
 
+  expect_identical(tester$opinion_type[1], "Per Curiam")
+  expect_identical(tester$opinion_type[4], "Dissenting")
+
+  expect_identical(tester$author[1], "Per Curiam")
+  expect_identical(tester$author[4], "Breyer")
+})
+
+test_that("scrape_optype returns a tibble containing accurate opinion_type column", {
+  expected <- tibble::tibble(opinion_type = c("Per Curiam", "Per Curiam", "Per Curiam", "Dissenting", "Dissenting",
+                "Dissenting", "Dissenting", "Dissenting", "Dissenting", "Dissenting"))
+
+  tester <- scrape_optype(test_pdf[[1]])
+
+  expect_identical(tester, expected)
+})
+
+test_that("scrape_author returns a tibble containing accurate author column", {
+  expected <- tibble::tibble(author = c("Per Curiam", "Per Curiam",
+                                              "Per Curiam", "Breyer", "Breyer",
+                                              "Breyer", "Sotomayor", "Sotomayor",
+                                              "Sotomayor", "Sotomayor"))
+  tester <- scrape_author(test_pdf[[1]])
+
+  expect_identical(tester, expected)
 })
 
 # test_that("scrape_cleaner ")
