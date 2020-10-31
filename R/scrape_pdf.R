06dfd4168e977e339865b332d090c0b92cc069b3
JCRascal <67589346+JCRascal@users.noreply.github.com>
2020-10-31 13:56:04 -0400

    scrape_pdf now adds the name of the case, passing all current unit tests; must add ability to handle opinion types other than dissenting and per curiam, to be handled in future commit; also handle documentation notes/warnings future commit

--- R/scrape_pdf.R
+++ R/scrape_pdf.R
@@ -1,3 +1,11 @@
+#' Scrape pdf for text
+#'
+#' Scrape the pages of text from pdfs containing Supreme Court opinions;
+#' cleans text and organizes relevant metadata
+#'
+#' @param .data
+#' @importFrom pdftools pdf_text
+
 scrape_pdf <- function(.data){
   text_og <- .data %>%
     tibble::tibble("text" = .)
@@ -5,8 +13,11 @@ scrape_pdf <- function(.data){
   text_col <- purrr::map_dfr(text_og, txt_clean)
   op_type_col <- scrape_optype(text_og)
   author_col <- scrape_author(text_og)
+  case_title <- text_og$text[[2]] %>%
+    stringr::str_extract("(?<=\\s).+(?=\\r\\n)") %>%
+    stringr::str_trim()
 
-  tibbleized_pdf <- tibble::tibble(text_col, op_type_col, author_col)
+  tibbleized_pdf <- tibble::tibble(text_col, "case_name" = case_title, op_type_col, author_col)
 
 }
 
@@ -33,7 +44,7 @@ scrape_1 <- function(main_url, page){
 scrape_optype <- function(.data){
   .data %>%
     #tibble::tibble("text" = .) %>%
-    purrr::map_dfr(stringr::str_trunc, 115) %>%
+    purrr::map_dfr(stringr::str_trunc, 200) %>%
     mutate(dissenting = stringr::str_detect(text, "dissenting")) %>%
     mutate(per_curiam = stringr::str_detect(text, "Per Curiam")) %>%
     mutate(opinion_type = if_else(dissenting == TRUE, true = "Dissenting", false = NA_character_)) %>%
@@ -45,7 +56,7 @@ scrape_optype <- function(.data){
 scrape_author <- function(.data){
   .data %>%
     #tibble::tibble("text" = .) %>%
-    purrr::map_dfr(stringr::str_trunc, 115) %>%
+    purrr::map_dfr(stringr::str_trunc, 200) %>%
     mutate(author = stringr::str_extract(text, ".+(?=, J., dissenting)")) %>%
     mutate(author = if_else(stringr::str_detect(text, "Per Curiam"), true = "Per Curiam", false = author)) %>%
     select(author) %>%
